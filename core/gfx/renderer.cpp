1030da42b0591ad49cca885cb4bdbcd8c06b3b72
Igor Alexey <igor@vortex-dev.com>
2023-10-15 13:24:55 -0300

    remove cached submodule

--- core/gfx/renderer.cpp
+++ core/gfx/renderer.cpp
@@ -7,6 +7,13 @@ float vertices[18] = {
     -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
     0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
 };
+
+float texCoords[] = {
+    0.0f, 0.0f, // lower-left corner
+    1.0f, 0.0f, // lower-right corner
+    0.5f, 1.0f  // top-center corner
+};
+
 unsigned int ID, VBO, VAO;
 //
 
