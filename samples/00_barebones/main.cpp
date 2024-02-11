4f276f2f91866f79cc0bf1f15cc15a0e2a49b0cf
IgorAlexey <igor@vortex-dev.com>
2024-02-10 21:56:17 -0300

    concise example

--- samples/00_barebones/main.cpp
+++ samples/00_barebones/main.cpp
@@ -4,17 +4,21 @@ using namespace Magnet;
 
 const char* const TITLE = "Barebones Example";
 class AppInterface : public ProjectInterface {
-public:
- void init(ApplicationContext& ctx) override {
-   // ArchiveManager archiveMgr("archive.magnet", "key")
+ public:
+  void init(ApplicationContext& ctx) override {
+    // ArchiveManager archiveMgr("archive.magnet", "key")
 
-   // Here you would use the ArchiveManager to load essential assets of your
-   // project, including shaders, textures, sounds, etc...
- }
+    // Here you would use your ArchiveManager to load essential assets of your
+    // project, including shaders, textures, sounds, etc...
 
- void update(ApplicationContext& ctx) override {
-   // ...
- }
+    // For quick logging, you can use the default logger which has a console (only DEBUG) and file (output.log) sinks.
+    spdlog::info("Hello, world!");
+
+  }
+
+  void update(ApplicationContext& ctx) override {
+    // ...
+  }
 };
 
 /* ENTRY POINT */
